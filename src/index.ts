c5fd57ff766d5609bfea440d6013eced3ba6f937
Michael John Angelo Belciña <mjabelcina.dev@gmail.com>
2025-12-06 04:17:43 +0800

    refactor: discontinue browser automation in Sik, use updated search-engine-scraper

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { Hono } from 'hono'
-import { ecosia, duckduckgo, createPatchrightClient } from 'search-engine-scraper'
+import { ecosia } from 'search-engine-scraper'
 import type { SERPResponse, SERPQueryParams } from 'search-engine-scraper'
 import { createCuimpClient } from 'search-engine-scraper'
 
@@ -12,18 +12,6 @@ app.post('/ecosia', async (c) => {
     const result: SERPResponse = await ecosia(createCuimpClient({}), body, {}, "")
 
     return c.json(result)
-})
-
-app.post("/duckduckgo", async (c) => {
-  const body: SERPQueryParams = await c.req.json()
-  const { page, browser } = await createPatchrightClient({ headless: true })
-
-  try {
-    const result: SERPResponse = await duckduckgo(page, body)
-    return c.json(result)
-  } finally {
-    await browser.close()
-  }
 })
 
 export default { 
